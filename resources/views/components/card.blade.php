6b0c7f23c319002c9a4935e12c2e3279c2183b6d
Rizki5846 <rizkiardiansyah584@gmail.com>
2023-10-28 20:24:01 +0700

    Perbaikan Layout ke 1

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,12 @@
+<div class=" float-left flex justify-center card w-full lg:w-96 shadow-xl bg-slate-200 m-3">
+    <figure class="px-10 pt-10">
+      <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+    </figure>
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
