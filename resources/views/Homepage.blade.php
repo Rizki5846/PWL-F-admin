3b2422424d8523003a7c9e154deb2de889c5a595
JoshuaDwiJulianto <mu56529@gmail.com>
2023-11-01 13:34:30 +0700

    pergantian kedua joshua

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -8,10 +8,24 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
 </head>
-    <x-Navbar/>
-    <x-Sidebar/>
-<body class="bg-white">
-  
-</body>
-    <x-Footer/>
-</html>
+    <body class="bg-slate-50">
+        <x-Navbar />
+        <div class="flex">
+            <div class="w-1/4"> 
+                <x-Sidebar />
+            </div>
+            <div > 
+                <x-Card />
+                <x-Card />
+                <x-Card />
+                <x-Card />
+                <x-Card />
+                <x-Card />
+            </div>
+        </div>
+    </body>
+    <div>
+        <x-Footer class="fixed bottom-0 w-full bg-gray-200 p-4">
+        </x-Footer>
+    </div>
+    </html>
